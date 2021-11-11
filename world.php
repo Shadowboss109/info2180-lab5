c15820df9276eb9da901ea13066b788a46cfc39b
Shadowboss109 <theodorebennett3@gmail.com>
2021-11-10 19:14:52 -0500

    Edited world.php to display info as a table

--- world.php
+++ world.php
@@ -14,8 +14,26 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>
-<ul>
+<table style="width:100%;  border:1px solid black">
+   <tr>
+    <th style="text-align:center; " >Name</th>
+    <th style="text-align:center;">Continent</th>
+    <th style="text-align:center; ">Independence</th>
+    <th style="text-align:center; "> Head of State</th>
+  </tr> 
+  </table> 
+ 
 <?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+  <table style="width:100%;  border:1px solid black">
+
+ 
+  <tr>
+    <td style=width:25%><?=$row['name']; ?></td>
+    <td style=width:25%><?=$row['continent']; ?></td>
+    <td style=width:25%><?=$row['independence_year']; ?></td>
+    <td style=width:25%><?=$row['head_of_state']; ?></td>
+    
+
+  </tr> 
+</table> 
 <?php endforeach; ?>
-</ul>
