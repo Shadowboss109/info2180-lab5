ebe1c5e40f4002c0ad3fa0f5a5a6f50c9758c9c1
Shadowboss109 <theodorebennett3@gmail.com>
2021-11-10 22:58:35 -0500

    Edited world.php to allow search for cities, Edited world.css

--- world.js
+++ world.js
@@ -1,9 +1,16 @@
 window.addEventListener('load', e=>{ 
     let lookup= $("#lookup");
     let country= $("#country");
-    console.log("Hi");
+
+    let cit= $("#city");
+ 
+
+
+
 
     lookup.on('click',e=>{
+       
+        
         let search={country:`${country.val()}`};
         $.ajax({
             type:"GET",
@@ -12,9 +19,33 @@ window.addEventListener('load', e=>{
             dataType: "html"
 
         }).done(response=>{
+            $("#result").empty();
             $("#result").append(response);
             
         }).fail(response=>{
+            $("#result").empty();
+            $("#result").append("Error");
+        })
+
+        e.preventDefault();
+
+
+    });
+    cit.on('click',e=>{
+        
+        let search={country:`${country.val()}`, context:"cities"};
+        $.ajax({
+            type:"GET",
+            url:"world.php",
+            data: search,
+            dataType: "html"
+
+        }).done(response=>{
+            $("#result").empty();
+            $("#result").append(response);
+            
+        }).fail(response=>{
+            $("#result").empty();
             $("#result").append("Error");
         })
 
--- world.php
+++ world.php
@@ -5,35 +5,65 @@ $password = 'password123';
 $dbname = 'world';
 
 
+
 $country= $_GET['country'];
+$context= $_GET['context'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//Cities for a specific country
+$cities = $conn->query("SELECT cities.id, cities.name,cities.country_code, cities.district, cities.population FROM  countries JOIN cities ON cities.country_code=countries.code WHERE countries.name = '$country';");
+$city = $cities->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>
-<table style="width:100%;  border:1px solid black">
+
+<?php if(isset($context)):?>
+  <table class="t1">
    <tr>
-    <th style="text-align:center; " >Name</th>
-    <th style="text-align:center;">Continent</th>
-    <th style="text-align:center; ">Independence</th>
-    <th style="text-align:center; "> Head of State</th>
+    <th class="look" >Name</th>
+    <th class="look">District</th>
+    <th class="look">Population</th>
+
+  </tr> 
+  </table> 
+  <?php foreach ($city as $row): ?>
+  <table class="t2">
+
+ 
+  <tr>
+    <td class="c"><?=$row['name']; ?></td>
+    <td class="c"><?=$row['district']; ?></td>
+    <td class="c"><?=$row['population']; ?></td>
+  
+  </tr> 
+</table> 
+<?php endforeach; ?>
+
+  <?php else:?>
+<table class="t3">
+   <tr>
+    <th class="look1" >Name</th>
+    <th class="look1">Continent</th>
+    <th class="look1">Independence</th>
+    <th class="look1"> Head of State</th>
   </tr> 
   </table> 
  
 <?php foreach ($results as $row): ?>
-  <table style="width:100%;  border:1px solid black">
+  <table class="t4">
 
  
   <tr>
-    <td style=width:25%><?=$row['name']; ?></td>
-    <td style=width:25%><?=$row['continent']; ?></td>
-    <td style=width:25%><?=$row['independence_year']; ?></td>
-    <td style=width:25%><?=$row['head_of_state']; ?></td>
+    <td class="s"><?=$row['name']; ?></td>
+    <td class="s"><?=$row['continent']; ?></td>
+    <td class="s"><?=$row['independence_year']; ?></td>
+    <td class="s"><?=$row['head_of_state']; ?></td>
     
 
   </tr> 
 </table> 
 <?php endforeach; ?>
+<?php endif;?>
